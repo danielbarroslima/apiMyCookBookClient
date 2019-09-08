fd7b08de01b4fa4263c206469cc1cef3f0fe1fd7
danielbarroslima <daniel.barroslm@gmail.com>
2019-09-08 20:43:48 -0300

    faraday continue ...

--- spec/requests/get_api_spec.rb
+++ spec/requests/get_api_spec.rb
@@ -1,63 +1,53 @@
 require 'rails_helper'
-#irei inserir o factorybot  para diminuir  o código 
 
 describe 'selling recipes by api and' do 
 	it 'successfully view all recipes' do 
-		conn = Faraday.new(url: 'http://localhost:4000/api/v1')
-		response = conn.get '/api/v1/recipes'
+		response = Faraday.get 'http://localhost:4000/api/v1/recipes'
 
-		expect(response.body).to include 'Bolodecenoura'
+		json_recipe_api = JSON.parse(response.body, symbolize_names: true)
+
+		expect(response.body).to include 'Bolo de abacaxi'
 		expect(response.status).to eq 200
 	end
 
 	it 'successfully in show recipe' do 
-		conn = Faraday.new(url: 'http://localhost:4000/api/v1')
-		response = conn.get '/api/v1/recipes/4'
+		response = Faraday.get'http://localhost:4000/api/v1/recipes/4'
 
-		expect(response.body).to include 'Bolodecenoura'
+		json_recipe_api = JSON.parse(response.body, symbolize_names: true)
+
+		expect(json_recipe_api[:title]).to include 'Bolo de abacaxi'
 		expect(response.status).to eq 200
 	end
 
 	it 'successfully in create recipe' do 
-		conn = Faraday.new(:url => 'http://localhost:4000/api/v1')
-		response = {}
-		conn.post do |req|
-		  req.url '/api/v1/recipes/',recipe:{title: 'teste', recipe_type:'teste', cuisine:'teste', cook_method:'teste', cook_time:43 , ingredients: 'teste', difficulty:'teste'}
-		  req.headers['Content-Type'] = 'application/json'
-		  req.body = '{"title": "teste", "recipe_type":"teste", "cuisine":"teste", "cook_method":"teste", "cook_time":43 , "ingredients": "teste", "difficulty":"teste"}'
-		  response = req.body 
-		end
+		url = 'http://localhost:4000/api/v1/recipes'
+		recipe = '{"title": "Bolo de banana", "difficulty": "Médio", "recipe_type": "brasileira", "cuisine": "brasileira", "cook_time": "50", "ingredients": "Farinha,açucar, cenoura", "cook_method": "Cozinhe a cenoura"}'
+		response = Faraday.post(url, recipe,"Content-Type" => "application/json")
 
-			expect(response).to include 'teste'
-			# expect(response) faltando a opção de status 
+		json_recipe_api = JSON.parse(response.body, symbolize_names: true)
+
+		expect(json_recipe_api[:title]).to include "Bolo de banana"
+		expect(response.status).to eq 201
 	end
 
 	it 'successfully in update recipe' do 
-		conn = Faraday.new(:url => 'http://localhost:4000/api/v1')
-		response = {}
-		conn.patch do |req|
-		  req.url '/api/v1/recipes/4',recipe:{title: 'Bolodecenoura', recipe_type:'teste2', cuisine:'teste', cook_method:'teste2', cook_time:43 , ingredients: 'teste', difficulty:'teste'}
-		  req.headers['Content-Type'] = 'application/json'
-		  req.body = '{"title": "teste2", "recipe_type":"teste2", "cuisine":"teste", "cook_method":"teste2", "cook_time":43 , "ingredients": "teste", "difficulty":"teste"}'
-		  response = req.body 
-		end
+		url = 'http://localhost:4000/api/v1/recipes/7'
+		recipe = '{"title": "Bolo de abacaxi"}'
+		response = Faraday.patch(url,recipe,"Content-Type" => "application/json")
 
-			expect(response).to include 'teste'
-			# expect(response) faltando a opção de status 
+		json_recipe_api = JSON.parse(response.body, symbolize_names: true)
+
+		expect(json_recipe_api[:title]).to include 'Bolo de abacaxi'
+		expect(response.status).to eq 202
 	end
 
 		it 'successfully in delete recipe' do 
-		conn = Faraday.new(:url => 'http://localhost:4000/api/v1')
-		response = {}
-		conn.delete do |req|
-		  req.url '/api/v1/recipes/3'
-		  req.headers['Content-Type'] = 'application/json'
-		  response = req.body 
-		end
+			response = Faraday.delete 'http://localhost:4000/api/v1/recipes/12'
 
-			# expect(response).to include 'teste' faltando o resultado  obtido
-			# expect(response) faltando a opção de status que é o mais importante 
+			expect(response.body).not_to include 'teste' 
+			expect(response.status).to eq 202
 	end
 
 end
+
 
